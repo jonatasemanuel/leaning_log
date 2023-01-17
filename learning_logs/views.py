dee47a95dde3266fc23402921e2f1c1ab9ffde20
Jonatas Silva <jonatassilvaemanuel@gmail.com>
2023-01-16 23:38:28 -0300

    topics ok this time

---
+++ learning_logs/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Topic
+
+
+# Create your views here.
+def index(request):
+    """A página inicial de Learning Log"""
+    return render(request, 'learning_logs/index.html')
+
+
+def topics(request):
+    """Mostra todos os assuntos."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
